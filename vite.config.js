87e2fc01478401f32a74335c85909919ab539ecd
Fanil Borkhanov <Fanil.Borkhanov@vmedia.ca>
2025-01-24 19:03:49 +0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "https://fanil116.github.io/aiscreen/",
+  base: "./",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
